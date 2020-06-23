f8edbbbdfb5b2f84ee8946bd17e8d0ead21249f3
patrick <patrick_sawyer@hotmail.co.uk>
2020-06-23 16:02:40 +0100

    Title appears on screen and tested

--- src/components/TikTokViewer/TikTokViewer.jsx
+++ src/components/TikTokViewer/TikTokViewer.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class TikTokViewer extends Component {
+  state = {
+    suffix: "https://vm.tiktok.com/JeJCFTp/",
+    title: null,
+    thumbnail_url: null,
+  };
+
+  componentDidMount = () => {
+    const prefix = "https://www.tiktok.com/oembed?url=";
+    axios.get(prefix + this.state.suffix).then((response) => {
+      this.setState({
+        title: response.data.title,
+        thumbnail_url: response.data.thumbnail_url,
+      });
+      console.log(this.state.thumbnail_url);
+    });
+  };
 
   render = () => {
     return (
-        <div className="tik-tok-viewer">
-          Hello world.
-        </div>
-      )
-  }
-
+      <div className="tik-tok-viewer" data-cy="tik-tok">
+        {this.state.title}
+      </div>
+    );
+  };
 }
 
 export default TikTokViewer;
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,1 +1,9 @@
-// Holds functions to handle API requests
+// // Holds functions to handle API requests
+// import axios from "axios";
+
+// tikTokApi = (suffix) => {
+//   const prefix = "https://www.tiktok.com/oembed?url=";
+//   axios.get(prefix + suffix);
+// };
+
+// export default tikTokApi;
