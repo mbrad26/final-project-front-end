75f5f441a1464c47c312713a380fdae47192523a
mbrad26 <mbrad26@gmail.com>
2020-06-25 11:27:36 +0100

    Implemented test for the Navabar Home link

--- src/components/Navbar/Navbar.spec.js
+++ src/components/Navbar/Navbar.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 
 describe('Navbar', () => {
@@ -11,5 +12,10 @@ describe('Navbar', () => {
 
   it('should render a nav', () => {
     expect(wrapper.find('nav').length).toEqual(1);
+  });
+
+  it('should have a home link', () => {
+    expect(wrapper.find(Link).at(0).props().to).toBe('/');
+    expect(wrapper.find(Link).at(0).text()).toBe('Home');
   });
 });
