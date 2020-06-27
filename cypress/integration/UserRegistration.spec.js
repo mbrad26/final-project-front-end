f7084cabc62e107c6c98229a328c1a4b4fab2327
mbrad26 <mbrad26@gmail.com>
2020-06-27 17:55:05 +0100

    Fixed user sign in and user registration feature tests;

--- cypress/integration/UserRegistration.spec.js
+++ cypress/integration/UserRegistration.spec.js
@@ -1,23 +1,23 @@
 describe('User registration', () => {
   it('register a new user account', () => {
-    cy.visit("http://localhost:3000")
+    cy.visit("http://localhost:3000");
 
-    cy.get('#register-account').click()
-    cy.location('pathname').should('eq', '/signup')
+    cy.get('#register-account').click();
+    cy.location('pathname').should('eq', '/signup');
 
-    cy.get('form')
-    cy.get('input[name="username"]').type('username')
-    cy.get('input[name="email"]').type('email@example.com')
-    cy.get('input[name="password"]').type('password')
-    cy.get('input[name="password_confirmation"]').type('password')
+    cy.get('form');
+    cy.get('input[name="username"]').type('username');
+    cy.get('input[name="email"]').type('email@example.com');
+    cy.get('input[name="password"]').type('password');
+    cy.get('input[name="password_confirmation"]').type('password');
     cy.server();
     cy.route(
       "POST",
-      "https://postman-echo.com/post",
+      'http://httpbin.org/post',
       'fixture:userRegistration'
     )
-    cy.get('.signup').click()
+    cy.get('.signup').click();
 
-    cy.location('pathname').should('eq', '/')
+    cy.location('pathname').should('eq', '/');
   });
 });
--- cypress/integration/UserSignIn.spec.js
+++ cypress/integration/UserSignIn.spec.js
@@ -2,19 +2,7 @@ describe('User sign in', () => {
   it('sign in', () => {
     cy.visit("http://localhost:3000");
 
-    cy.get('#register-account').click();
-    cy.location('pathname').should('eq', '/signup');
-
-    cy.get('form');
-    cy.get('input[name="username"]').type('username');
-    cy.get('input[name="email"]').type('email@example.com');
-    cy.get('input[name="password"]').type('password');
-    cy.get('input[name="password_confirmation"]').type('password');
-    cy.get('.signup').click();
-
-    cy.location('pathname').should('eq', '/');
-
-    cy.get('form');
+    cy.get('form.signin-form');
     cy.get('input[name="username"]').type('username');
     cy.get('input[name="password"]').type('password');
     cy.get('.signin').click();
