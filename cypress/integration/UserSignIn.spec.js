3a1a1b35d68576c71168a987dec4656a944a3c14
mbrad26 <mbrad26@gmail.com>
2020-06-28 12:22:10 +0100

    Feature test for user sign out

--- cypress/integration/UserSignIn.spec.js
+++ cypress/integration/UserSignIn.spec.js
@@ -1,1 +1,12 @@
+describe('User Sign In', () => {
+  it('a user can sign in', () => {
+    cy.visit("http://localhost:3000");
 
+    cy.get('form.signin-form');
+    cy.get('input[name="username"]').type('username');
+    cy.get('input[name="password"]').type('password');
+    cy.get('#signin').click();
+
+    cy.location('pathname').should('eq', '/account');
+  });
+});
