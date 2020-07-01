7e0fc4d4985f7e024057d5692d23456b009710ec
@willhead <60652390+will-head@users.noreply.github.com>
2020-07-01 15:16:21 +0100

    Adds react-bootstrap

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
