6e8b086aeb8c5cefbbcafefb0d0a9fa0b1c241b7
mbrad26 <mbrad26@gmail.com>
2020-07-02 10:10:46 +0100

    Fixed user sign in feature test

--- cypress/integration/UserSignIn.spec.js
+++ cypress/integration/UserSignIn.spec.js
@@ -1,6 +1,6 @@
 describe('User Sign In', () => {
   it('a user can sign in', () => {
-    cy.visit("http://localhost:30001/sessions");
+    cy.visit("http://localhost:3000");
 
     cy.get('form.signin-form');
     cy.get('input[name="username"]').type('username');
