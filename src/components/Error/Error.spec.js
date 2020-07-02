5e710d1979abd24c897274cf17916af1bd003e87
patrick <patrick_sawyer@hotmail.co.uk>
2020-07-02 18:49:52 +0100

    ready

--- src/components/Error/Error.spec.js
+++ src/components/Error/Error.spec.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Error from './Error';
+import React from "react";
+import { shallow } from "enzyme";
+import Error from "./Error";
 
-describe('Error', () => {
+describe("Error", () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(<Error />);
   });
 
-  it('should render a div', () => {
-    expect(wrapper.find('div').length).toEqual(1);
+  it("should render a div", () => {
+    expect(wrapper.find("div.error-container").length).toEqual(1);
   });
 
-  it('should render an error message', () => {
-    let error = 'Nothing here for you to see!';
+  it("should render an error message", () => {
+    let error = "Nothing here for you to see!";
 
-    expect(wrapper.find('h1').text()).toEqual(error);
+    expect(wrapper.find("p").text()).toEqual(error);
   });
 });
