5395fa1a7c6d6a2fc225e881108e2096208a1239
patrick <patrick_sawyer@hotmail.co.uk>
2020-07-03 10:50:04 +0100

    scroll fix

--- src/components/TikTokViewer/TikTokViewer.jsx
+++ src/components/TikTokViewer/TikTokViewer.jsx
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import axios from "axios";
 import Buttons from "../Buttons/Buttons";
 import "./TikTokViewer.css";
-import { faPlay, faPause, faVolumeUp, faVolumeMute, faCaretUp, faCaretDown, faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPlay,
+  faPause,
+  faVolumeUp,
+  faVolumeMute,
+  faCaretUp,
+  faCaretDown,
+  faCaretLeft,
+  faCaretRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 class TikTokViewer extends Component {
   state = {
@@ -65,12 +73,14 @@ class TikTokViewer extends Component {
 
   playVideo() {
     this.setState({
-      playing: !this.state.playing
+      playing: !this.state.playing,
     });
     // this.state.playing ? this.refs.vidRef.muted=false : this.refs.vidRef.muted=true;
     this.state.playing ? this.refs.vidRef.pause() : this.refs.vidRef.play();
     // this.state.playing ? this.refs.vidRef.volume = 1 : this.refs.vidRef.volume = 0;
-    this.state.playing ? this.state.playPause=">" : this.state.playPause="||"
+    this.state.playing
+      ? (this.state.playPause = ">")
+      : (this.state.playPause = "||");
     // this.state.playing ? this.state.playPause="<i className='fas fa-volume-up'></i>" : this.state.playPause="<i className='fas fa-volume-mute'></i>"
     // this.state.playing ? this.state.playPause="MUTE" : this.state.playPause="UNMUTE"
     // this.refs.vidRef.play();
@@ -78,63 +88,95 @@ class TikTokViewer extends Component {
 
   muteVideo() {
     this.setState({
-      muted: !this.state.muted
+      muted: !this.state.muted,
     });
-    this.state.muted ? this.refs.vidRef.muted=false : this.refs.vidRef.muted=true;
+    this.state.muted
+      ? (this.refs.vidRef.muted = false)
+      : (this.refs.vidRef.muted = true);
     // this.state.muted ? this.state.muteUnmute="M" : this.state.muteUnmute="U"
-    console.log()
+    console.log();
   }
 
-        // <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>PLAY</button>
-        // <button className="play btn-block btn-lg" onClick={this.muteVideo.bind(this)}>{this.state.muteUnmute}</button>
-// <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
-        // <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
-        //         <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
+  // <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>PLAY</button>
+  // <button className="play btn-block btn-lg" onClick={this.muteVideo.bind(this)}>{this.state.muteUnmute}</button>
+  // <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
+  // <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
+  //         <button className="play btn-block btn-lg" onClick={this.playVideo.bind(this)}>{this.state.playPause}</button>
 
-        // <button type="button" className="previous btn btn-block btn-lg" onClick={this.handlePrevious}><i class="arrow up"></i></button>  
-        // <button className="next btn btn-block btn-lg" onClick={this.handleNext}><i class="arrow down"></i></button>
+  // <button type="button" className="previous btn btn-block btn-lg" onClick={this.handlePrevious}><i class="arrow up"></i></button>
+  // <button className="next btn btn-block btn-lg" onClick={this.handleNext}><i class="arrow down"></i></button>
 
   showMute() {
-    if (this.state.muted === true){
-      return (<FontAwesomeIcon icon={faVolumeMute} />)
+    if (this.state.muted === true) {
+      return <FontAwesomeIcon icon={faVolumeMute} />;
     } else {
-      return (<FontAwesomeIcon icon={faVolumeUp} />)
+      return <FontAwesomeIcon icon={faVolumeUp} />;
     }
   }
 
   showPlay() {
-    if (this.state.playing === true){
-      return (<FontAwesomeIcon icon={faPause} />)
+    if (this.state.playing === true) {
+      return <FontAwesomeIcon icon={faPause} />;
     } else {
-      return (<FontAwesomeIcon icon={faPlay} />)
+      return <FontAwesomeIcon icon={faPlay} />;
     }
   }
 
   render = () => {
     return (
       <div className="tik-tok-viewer" data-cy="tik-tok">
-        <div className="content">  
+        <div className="content">
           <div className="embed-responsive embed-responsive-9by16 ">
             <div className="bottom-fade">
-            <video className="embed-responsive-item " ref="vidRef" src={this.state.src} loop playsInline autoPlay muted />
+              <video
+                className="embed-responsive-item "
+                ref="vidRef"
+                src={this.state.src}
+                loop
+                playsInline
+                autoPlay
+                muted
+              />
             </div>
-            <div className="tik-tok-title bottom-left">{this.state.title}<div className="title-spacer"></div></div>
+            <div className="tik-tok-title bottom-left">
+              {this.state.title}
+              <div className="title-spacer"></div>
+            </div>
             <div className="controls">
-              <p className="previous top-center" onClick={this.handlePrevious}><FontAwesomeIcon icon={faCaretUp} /></p>
-              <p className=" left-center side" onClick={this.handlePrevious}><FontAwesomeIcon icon={faCaretLeft} /></p>
-              <p className="next bottom-center" onClick={this.handleNext}><FontAwesomeIcon icon={faCaretDown} /></p>
-              <p className="right-center side" onClick={this.handleNext}><FontAwesomeIcon icon={faCaretRight} /></p>
-              <p className=" play centered" onClick={this.playVideo.bind(this)}>{this.showPlay()}</p>
-              <div className="mute-container top-right"><p className=" mute " onClick={this.muteVideo.bind(this)}>{this.showMute()}</p></div>
+              <p className="previous top-center" onClick={this.handlePrevious}>
+                <FontAwesomeIcon icon={faCaretUp} />
+              </p>
+              <p className=" left-center side" onClick={this.handlePrevious}>
+                <FontAwesomeIcon icon={faCaretLeft} />
+              </p>
+              <p className="next bottom-center" onClick={this.handleNext}>
+                <FontAwesomeIcon icon={faCaretDown} />
+              </p>
+              <p className="right-center side" onClick={this.handleNext}>
+                <FontAwesomeIcon icon={faCaretRight} />
+              </p>
+              <p className=" play centered" onClick={this.playVideo.bind(this)}>
+                {this.showPlay()}
+              </p>
+              <div className="mute-container top-right">
+                <p className=" mute " onClick={this.muteVideo.bind(this)}>
+                  {this.showMute()}
+                </p>
               </div>
-          </div>   
-         
+            </div>
+          </div>
         </div>
         <style>
           {
             "\
           body{\
             background: black !important;\
+            overflow: hidden;\
+            overflow: -moz-scrollbars-none;\
+            -ms-overflow-style: none;\
+          }\
+          body::-webkit-scrollbar {\
+            width: 0 !important;\
           }"
           }
         </style>
